bbe7a581b62f5217dd98b70df42db0c09dd30cd3
valentin <valentin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-22 13:09:02 +0000

    change in codeGeno, added function for LD heatmap plot, new version 0.1-3
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/synbreed/R/codeGeno.r
+++ pkg/synbreed/R/codeGeno.r
@@ -37,13 +37,14 @@ codeGeno <- function(data,impute=FALSE,popStruc=NULL,maf=NULL,nmiss=NULL){
   
   # number of missin values
   nmv <- sum(is.na(res))
+   # initialize counter  
+          cnt1 <- 0    # for nr. of imputations with family structure
+          cnt2 <- 0    # for nr. of random imputations
 
   # impute missing values according to population structure
   if(impute & !is.null(popStruc)){
   
-     # initialize counter  
-          cnt1 <- 0    # for nr. of imputations with family structure
-          cnt2 <- 0    # for nr. of random imputations
+    
   
     # loop over all markers
     for (j in 1:M){
@@ -98,8 +99,10 @@ codeGeno <- function(data,impute=FALSE,popStruc=NULL,maf=NULL,nmiss=NULL){
    # impute missing values with no population structure
     if(impute & is.null(popStruc)){
         for (j in 1:M){
+             cnt2 <- cnt2 + sum(is.na(res[,j]))
              if(j==1) ptm <- proc.time()[3]
-             res[is.na(res[,j]),j] <- sample(c(0,2),size=sum(is.na(res[,j])),prob=table(res[,j])/n,replace=TRUE)
+              if (length(table(res[,j]))==1)   res[is.na(res[,j]),j] <- 0
+              else res[is.na(res[,j]),j] <- sample(c(0,2),size=sum(is.na(res[,j])),prob=table(res[,j])/n,replace=TRUE)
              if(j==1) cat("Approximative run time ",(proc.time()[3] - ptm)*M," seconds \n",sep=" ")
         }   
    }    
