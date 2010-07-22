4cb9f0beee9a8bb667efe8170ceab81bf94c340e
valentin <valentin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-22 13:25:02 +0000

    change in LDMap
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/synbreed/R/LDMap.r
+++ pkg/synbreed/R/LDMap.r
@@ -2,11 +2,10 @@ LDMap <- function(marker,linkageGroup,pos,file=NULL,...){
      # Calculating LD as r2
      ld.r <- cor(marker,method="spearman")^2
      lg <- unique(linkageGroup)
+     if(!is.null(file)) pdf(file)
      for (i in 1:length(lg)){
       LDheatmap(ld.r[linkageGroup == lg[i],linkageGroup == lg[i]], LDmeasure="r",title=paste("Pairwise LD r2, Linkage Group ",i,sep=""), color=brewer.pal(9,"OrRd")[9:1], genetic.distances=pos[linkageGroup == lg[i]], geneMapLabelY=0.12, geneMapLabelX=0.35, distances = "genetic",...)
      }
-     if(!is.null(file))
+     if(!is.null(file)) dev.off()
 }
 
-data(maize)
-LDMap(maize.geno[,-1],maize.marker.pos$chr,maize.marker.pos$pos)
