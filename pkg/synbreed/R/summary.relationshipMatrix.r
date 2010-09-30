911a64df11004159946b641cccdffa1c52089ea1
valentin <valentin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-30 14:08:46 +0000

    minor changes in vignette and documentation bug fix in computing rank in summary.relationshipmatrix (wrong results in rare cases by old version, new version based on qr decompostion)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/synbreed/R/summary.relationshipMatrix.r
+++ pkg/synbreed/R/summary.relationshipMatrix.r
@@ -1,7 +1,7 @@
 summary.relationshipMatrix <- function(relationshipMatrix){
       relMat <- relationshipMatrix
      cat("Dimension         :",nrow(relMat),"x",ncol(relMat),"\n")
-     cat("Rank              :",sum(diag(relMat%*%ginv(relMat))),"\n")
+     cat("Rank              :",qr(relMat)$rank,"\n")
      cat("Range             :",min(relMat),"--",max(relMat),"\n")
      cat("# of unique values:",length(unique(relMat[upper.tri(relMat,diag=TRUE)])),"\n")
 }
