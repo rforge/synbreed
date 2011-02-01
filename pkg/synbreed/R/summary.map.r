170a5143ef963ea801f7318ef71c54a192ac3a1b
valentin <valentin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-01 08:34:55 +0000

    missing ads. for Rev. 91
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@92 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/synbreed/R/summary.map.r
@@ -0,0 +1,25 @@
+# summary for maker maps
+
+summary.map <- function(map){
+
+     # information from arguments
+     if(class(map)=="gpData") map <- map$map
+     chr <- map$chr
+     pos <- map$pos
+
+     # extract information
+     # number of markers
+     len <- tapply(pos,chr,length)
+     rge <- tapply(pos,chr,max,na.rm=TRUE)-tapply(pos,chr,min,na.rm=TRUE)
+
+     # differences of markers on each chr
+     diffs <- tapply(pos[!is.na(pos)],chr[!is.na(pos)],diff,na.rm=TRUE)
+     avDist <- as.numeric(lapply(diffs,mean,na.rm=TRUE))
+     maxDist <- as.numeric(lapply(diffs,max,na.rm=TRUE))
+     minDist <- as.numeric(lapply(diffs,min,na.rm=TRUE))
+
+     # return data.frame
+     ret <- data.frame(noM = len, range=rge, avDist = avDist, maxDist = maxDist, minDist = minDist,row.names=names(len))
+     return(ret)
+
+}
