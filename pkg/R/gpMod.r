3663b75dde3edaa00af1e3040662a41a448f23bd
valentin <valentin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-03 11:22:29 +0000

    added function gpMod
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@140 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/gpMod.r
@@ -0,0 +1,80 @@
+# wrapper to apply genomic prediction models to an object of class gpData
+# models used: regress and BLR
+
+# author: Valentin Wimmer
+# date: 2011 - 05 - 03
+
+gpMod <- function(gpData,model=c("BLUP","BL","BRR"),kin=NULL,trait=1,...){
+    model <- match.arg(model)
+
+    # inidividuls with genotypes and phenotypes
+    trainSet <- as.character(gpData$covar$id[gpData$covar$genotyped & gpData$covar$phenotyped])
+    n <- length(trainSet)
+
+    # take data from gpData object
+    y <- gpData$pheno[rownames(gpData$pheno) %in% trainSet,trait]
+
+    if (model=="BLUP"){
+      if (is.null(kin)) stop("Missing object 'kin'")
+      if (n!=nrow(kin)){
+         kin <- kin[rownames(kin) %in% trainSet,rownames(kin) %in% trainSet]
+      }
+      res <- regress(y~1,Vformula=~kin,...)
+      genVal <- res$predicted
+    }
+    if (model=="BL"){
+      X <- gpData$geno[rownames(gpData$geno) %in% trainSet,]
+      res <- BLR(y=y,XL=X,...)
+      if(!is.null(kin)) res <- BLR(y=y,XL=X,GF=list(ID=1:n,A=kin),...)
+      genVal <- res$yHat
+    }
+    if (model=="BRR"){
+      X <- gpData$geno[rownames(gpData$geno) %in% trainSet,]
+      res <- BLR(y=y,XR=X,...)
+      if(!is.null(kin)) res <- BLR(y=y,XR=X,GF=list(ID=1:n,A=kin),...)
+      genVal <- res$yHat
+    }
+
+
+    ret <- list(fit=res,model=model,y=y,g=genVal)
+    class(ret) = "gpMod"
+    return(ret)
+
+}
+
+summary.gpMod <- function(object,...){
+    ans <- list()
+    ans$model <- object$model
+    if(object$model=="BLUP") ans$summaryFit <- summary(object$fit)
+    if(object$model=="BL") ans$summaryFit <- list(mu = object$fit$mu, varE=object$fit$varE, lambda=object$fit$lambda, nIter = object$fit$nIter,burnIn = object$fit$burnIn,thin=object$fit$thin)
+    if(object$model=="BRR") ans$summaryFit <- list(mu = object$fit$mu, varE=object$fit$varE, varBr=object$fit$varBr, nIter = object$fit$nIter,burnIn = object$fit$burnIn,thin=object$fit$thin)
+    ans$n <- length(object$y)
+    ans$sumNA <- sum(is.na(object$y))
+    ans$summaryG <- summary(as.numeric(object$g))
+    class(ans) <- "summary.gpMod"
+    ans
+}
+
+print.summary.gpMod <- function(x,...){
+    cat("object of class 'gpMod' \n")
+    cat("Model used:",x$model,"\n")
+    cat("nr. observations ",x$n," (NA = ",x$sumNA,") \n",sep="")
+    cat("Genetic values: \n")
+    cat("Min. 1st Qu. Median Mean 3rd Qu. Max \n")
+    cat(x$summaryG, " \n")
+    cat("--\n")
+    cat("Model fit \n")
+    if(x$model=="BLUP") cat(print(x$summaryFit),"\n")
+    else{
+    cat("MCMC options: nIter = ",x$summaryFit$nIter,", burnIn = ",x$summaryFit$burnIn,", thin = ",x$summaryFit$thin,"\n",sep="")
+    cat("             Posterior mean \n")
+    cat("(Intercept) ",x$summaryFit$mu,"\n")
+    cat("VarE        ",x$summaryFit$varE,"\n")
+    if(x$model=="BL"){
+    cat("lambda      ",x$summaryFit$lambda,"\n")
+    }
+    if(x$model=="BRR"){
+    cat("varBr       ",x$summaryFit$varBr,"\n")
+    }
+    }
+}
