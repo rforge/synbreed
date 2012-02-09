8e6234b28a4b4757e8a16c4ce1f52c756fd18a3a
aui <aui@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-09 13:40:56 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@332 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/codeGeno.r
+++ pkg/R/codeGeno.r
@@ -181,7 +181,9 @@ codeGeno <- function(gpData,impute=FALSE,impute.type=c("random","family","beagle
     if(!is.null(maf)){
       if(maf<0 | maf>1) stop("'maf' must be in [0,1]")
       nv <- ncol(res)
-      res <- res[, colMeans(res, na.rm=TRUE)!=0]
+      part <- round(seq(0,nv, length.out=ceiling(nv/50000)+1))
+      for(i in length(part):2)
+         res <- res[, part[i-1]+((part[i-1]+1):part[i])[colMeans(res, na.rm=TRUE)!=0]]
       nv <- nv-ncol(res)
     }
     which.duplicated <- duplicated(res,MARGIN=2)
