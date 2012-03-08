8290d9204e5b0da90f25a81260b38a1c3b861728
theresa <theresa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-08 15:07:51 +0000

    if (verbose) if(nv>0) cat(         and 30 monomorphic marker(s))
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@348 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/codeGeno.r
+++ pkg/R/codeGeno.r
@@ -214,7 +214,7 @@ codeGeno <- function(gpData,impute=FALSE,impute.type=c("random","family","beagle
     res <- res[,!which.duplicated]
     cnames <- cnames[!which.duplicated]
     if (verbose) cat("step 2.1:",sum(which.duplicated)+step1,"duplicated marker(s) removed \n")
-    if(nv>0) cat("         and", nv, "monomorphic marker(s)\n")
+    if (verbose) if(nv>0) cat("         and", nv, "monomorphic marker(s)\n")
     # update map 
     if(!is.null(gpData$map)) gpData$map <- gpData$map[!which.duplicated,]
   } else {
