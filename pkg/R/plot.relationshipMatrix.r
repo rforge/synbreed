6fc4f07e0e9eeb50ba91f0f4c97403f2666681f0
aui <aui@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-17 16:33:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@413 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.relationshipMatrix.r
+++ pkg/R/plot.relationshipMatrix.r
@@ -1,6 +1,6 @@
 # heatmap for relationshipMatrix objects
 
-plot.relationshipMatrix <- function(x, file=NULL, fileFormat="pdf",...){
+plot.relationshipMatrix <- function(x,...){
 
     relMat <- x
     class(relMat) <- "matrix"
@@ -11,7 +11,5 @@ plot.relationshipMatrix <- function(x, file=NULL, fileFormat="pdf",...){
       levelplot(relMat,axes=FALSE,col.regions=color,cuts=8,xlab="",ylab="",scales=list(cex=1-(size-20)/50,rot=c(40,0),abbreviate=TRUE,minlength=5),ylim=c(size+1,0),...)
     } else {
       levelplot(relMat,axes=FALSE,col.regions=color,cuts=8,xlab="",ylab="",scales=list(at=c(1,size/2,size),labels=c(1,"...",paste(size)),tck=0),ylim=c(size+1,0),...)  
-    }
-    if(!is.null(file)) dev.off()
-                                   
+    }                                   
 }
