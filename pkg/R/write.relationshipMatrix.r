a4c73e4b307f663ce67aef1030762d4e5cb2c02c
aui <aui@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-03 13:50:27 +0000

    new names of the header in write.relationshipMatrix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@443 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/write.relationshipMatrix.r
+++ pkg/R/write.relationshipMatrix.r
@@ -7,28 +7,28 @@ write.relationshipMatrix <- function(relationshipMatrix,file=NULL,sorting=c("WOM
         
         # pass (inverse) relationship matrix
         if(type=="ginv") rMinv <- ginv(relationshipMatrix)
-      	if(type=="inv")  rMinv <- solve(relationshipMatrix)
+        if(type=="inv")  rMinv <- solve(relationshipMatrix)
         if(type=="none") rMinv <- relationshipMatrix
         
         rMinv <- round(rMinv,digits)
         
         # add rownames and colnames
-        res <- data.frame(coeff = as.numeric(rMinv),
-                        rowname = rep(1:nrow(rMinv), nrow(rMinv)),
-                        colname = rep(1:nrow(rMinv), each = nrow(rMinv)),
-                        lower = as.logical(lower.tri(rMinv, diag = TRUE)))
+        res <- data.frame(Row = rep(1:nrow(rMinv), nrow(rMinv)),
+                          Column = rep(1:nrow(rMinv), each = nrow(rMinv)),
+                          coeff = as.numeric(rMinv),
+                          lower = as.logical(lower.tri(rMinv, diag = TRUE)))
                         
       
     
         # only use lower triangle
-        res <- res[res$lower == TRUE, c("rowname", "colname", "coeff")]
+        res <- res[res$lower == TRUE, c("Row", "Column", "coeff")]
           
         if (sorting=="ASReml"){    
-          res <-  res[order( res$rowname,  res$colname), ] 
+          res <-  res[order( res$Row,  res$Column), ] 
         }
         if (sorting=="WOMBAT"){
           res <- res[,c(2,1,3)]
-          res <-  res[order( res$colname,  res$rowname), ]  
+          res <-  res[order(res$Column,  res$Row), ]  
         }
         
         # write to given file
