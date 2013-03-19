6987127e34e3f788f2250d5e6f1920f533193b7f
aui <aui@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-19 12:37:47 +0000

    fixed bug in add.individuals()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@464 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/add.markers.r
+++ pkg/R/add.markers.r
@@ -68,8 +68,8 @@ add.individuals <- function(gpData,pheno=NULL,geno=NULL,pedigree=NULL,covar=NULL
         if(!"ID" %in% colnames(pheno))  stop("In", substitute(pheno), "the columns 'ID' and/or 'repl' are/is missing!")
       }
       if(!is.null(pheno))
-        if(any(!colnames(pheno)[colnames(pheno)!="ID"]%in%dimnames(gpData$pheno)[[2]] |
-               !colnames(pheno)[colnames(pheno)!="ID"]%in%dimnames(gpData$phenoCovars)[[2]]))
+        if(!all(!colnames(pheno)[colnames(pheno)!="ID"]%in%dimnames(gpData$pheno)[[2]] |
+                !colnames(pheno)[colnames(pheno)!="ID"]%in%dimnames(gpData$phenoCovars)[[2]]))
           stop("different phenotypes (colnames) in '", substitute(gpData$pheno), "' or '",  substitute(gpData$phenoCovar),"' and '", substitute(pheno), "'")
       df.pheno <- gpData2data.frame(gpData, onlyPheno=TRUE, trait = dimnames(gpData$pheno)[[2]])
       if(!all(colnames(df.pheno) %in% colnames(pheno))) warning("Not all traits and covariates are available in the new data!")
