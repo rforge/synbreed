13a03fdcbd4e4762fe4b6aef5f55ad31094cf574
aui <aui@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-11-02 12:25:48 +0000

    fixed bug in LDmat summary
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@627 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/summary.LD.r
+++ pkg/R/summary.LD.r
@@ -10,8 +10,14 @@ summary.LDdf <- function(object,...){
      pr <- function(x) mean(x$r2>0.2,na.rm=TRUE)
      maxd <- function(x) max(x$dist,na.rm=TRUE)
 
-     ret <- data.frame(noM=unlist(lapply(LDdf,noM)),avgr2= unlist(lapply(LDdf,avgr2)),minr2= unlist(lapply(LDdf,minr2)),maxr2= unlist(lapply(LDdf,maxr2)),Pr02=unlist(lapply(LDdf,pr)),maxDist = unlist(lapply(LDdf,maxd)))
-     return(ret)   
+     ret <- data.frame(noM=unlist(lapply(LDdf,noM)),
+                       avgr2= unlist(lapply(LDdf,avgr2)),
+                       minr2= unlist(lapply(LDdf,minr2)),
+                       maxr2= unlist(lapply(LDdf,maxr2)),
+                       Pr02=unlist(lapply(LDdf,pr)),
+                       averDist = unlist(lapply(LDdf,avgr2)),
+                       maxDist = unlist(lapply(LDdf,maxd)))
+     return(ret)
 }
 
 summary.LDmat <- function(object,...){
@@ -22,8 +28,13 @@ summary.LDmat <- function(object,...){
      maxr2 <- function(x) max(x[upper.tri(x)],na.rm=TRUE)
      pr <- function(x) mean(x[upper.tri(x)]>0.2,na.rm=TRUE)
 
-     
-     ret <- data.frame(noM=unlist(lapply(LDmat$LD,ncol)),avgr2= unlist(lapply(LDmat$LD,avgr2)),minr2= unlist(lapply(LDmat$LD,minr2)),maxr2= unlist(lapply(LDmat$LD,maxr2)),Pr02=unlist(lapply(LDmat$LD,pr)),maxDist = unlist(lapply(LDmat$distance,avgr2)))
-     return(ret)   
-}
 
+     ret <- data.frame(noM=unlist(lapply(LDmat$LD,ncol)),
+                       avgr2= unlist(lapply(LDmat$LD,avgr2)),
+                       minr2= unlist(lapply(LDmat$LD,minr2)),
+                       maxr2= unlist(lapply(LDmat$LD,maxr2)),
+                       Pr02=unlist(lapply(LDmat$LD,pr)),
+                       averDist = unlist(lapply(LDmat$distance,avgr2)),
+                       maxDist = unlist(lapply(LDmat$distance,max)))
+     return(ret)
+}
