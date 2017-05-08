d759b2ece8d23dd12e6a7db62f7c1944dd3db8f9
aui <aui@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-05-08 18:55:43 +0000

    bug fixed for recoding gpData with kept allele frequencies
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@690 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/codeGeno.r
+++ pkg/R/codeGeno.r
@@ -765,8 +765,5 @@ codeGeno <- function(gpData,impute=FALSE,impute.type=c("random","family","beagle
    }
 
   # return a gpData object (or a matrix)
-  if(reference.allele[1]=="keep"){
-    gpData$geno[, cnames%in%afCols] <-  rep(1, nrow(gpData$geno)) %*% t(rep(2, sum(cnames%in%afCols))) - gpData$geno[, cnames%in%afCols]
-  }
   return(gpData)
 }
