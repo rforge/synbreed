dc6a4b4f86a28e38c71ddf3a6b90e7880874f78a
aui <aui@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-03-15 07:17:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@722 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/add.gpData.r
+++ pkg/R/add.gpData.r
@@ -7,11 +7,13 @@
 #gpData2$pedigree$ID[gpData2$pedigree$ID %in% rownames(gpData2$geno)] <- paste(gpData2$pedigree$ID[gpData2$pedigree$ID %in% rownames(gpData2$geno)], 0, sep="")
 #rownames(gpData2$geno) <- paste(rownames(gpData2$geno), 0, sep="")
 #dimnames(gpData2$pheno)[[1]] <- paste(dimnames(gpData2$pheno)[[1]], 0, sep="")
+#gpData2$pheno <- abind(gpData2$pheno, gpData2$pheno, along =2)
+#dimnames(gpData2$pheno)[[2]] <- c("IDTrait", "Trait2")
 
 add.gpData <- function(gpData1, gpData2){
-  if(!is.null(gpData$info$version)) stop(paste("Recode ", substitute(gpData1), "! You have used an old version to create/code ", substitute(gpData1), sep=""))
+  if(!is.null(gpData1$info$version)) stop(paste("Recode ", substitute(gpData1), "! You have used an old version to create/code ", substitute(gpData1), sep=""))
   if(substr(gpData1$info$version, 47, 50)<0.12) stop(paste("Recode ", substitute(gpData1), "! You have used an old version to create/code ", substitute(gpData1), sep=""))
-  if(!is.null(gpData$info$version)) stop(paste("Recode ", substitute(gpData1), "! You have used an old version to create/code ", substitute(gpData1), sep=""))
+  if(!is.null(gpData2$info$version)) stop(paste("Recode ", substitute(gpData1), "! You have used an old version to create/code ", substitute(gpData1), sep=""))
   if(substr(gpData2$info$version, 47, 50)<0.12) stop(paste("Recode ", substitute(gpData2), "! You have used an old version to create/code ", substitute(gpData2), sep=""))
   if(!is.null(gpData1$pheno))
     pheno1 <- gpData2data.frame(gpData1, onlyPheno=TRUE, trait=1:dim(gpData1$pheno)[2], stringsAsFactors=TRUE) else pheno1 <- NULL
