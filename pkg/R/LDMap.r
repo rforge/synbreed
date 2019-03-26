f6992fe9a52817e10bcebe4c2fdd3f927741b029
valentin <valentin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-03-26 16:26:26 +0000

    use LDheatmap again
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/synbreed@729 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/LDMap.r
+++ pkg/R/LDMap.r
@@ -37,8 +37,8 @@ LDMap <- function(LDmat,gpData,chr=NULL,file=NULL,fileFormat="pdf",onefile=TRUE,
         color = c("#7F0000","#B30000","#D7301F","#EF6548","#FC8D59","#FDBB84","#FDD49E","#FEE8C8","#FFF7EC")
         #   using function LDheatmap
         MapUnit <- ifelse(gpData$info$map.unit=="cM","genetics","physical")
-#        LDheatmap(LDmat$LD[[i]], LDmeasure="r", color=color, genetic.distances=pos[rownames(LDmat$LD[[i]])],distances=MapUnit,
-#                  geneMapLabelY=0.12, geneMapLabelX=0.35, title=paste("Pairwise LD on chromosome", i),...)
+        LDheatmap(LDmat$LD[[i]], LDmeasure="r", color=color, genetic.distances=pos[rownames(LDmat$LD[[i]])],distances=MapUnit,
+                  geneMapLabelY=0.12, geneMapLabelX=0.35, title=paste("Pairwise LD on chromosome", i),...)
         if(!is.null(file)&(fileFormat != "pdf" | fileFormat == "pdf" & !onefile)){
           dev.off()
         } else if(is.null(file) & length(lg)>1) readline()
